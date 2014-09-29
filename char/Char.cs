3681ce9bc4d236d81a01a968afca17016b6509a5
tadokoro <hiroshi.tadokoro@gu3.co.jp>
2014-09-29 18:14:24 +0900

    add Char

---
+++ char/Char.cs
@@ -0,0 +1,13 @@
+using System;
+
+class Char
+{
+    public static void Main()
+    {
+        Console.WriteLine("{0}", (int)'A');
+        Console.WriteLine("{0}", (int)'\0');
+        Console.WriteLine("{0}", (int)'\r');
+        Console.WriteLine("{0}", (int)'\n');
+        Console.WriteLine("{0}", (int)' ');
+    }
+}
