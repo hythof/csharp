14d3746f82aac50db7dbd2233ad748628b760040
tadokoro <hiroshi.tadokoro@gu3.co.jp>
2015-09-06 20:25:24 +0900

    add wait

---
+++ experimentation/task/Wait.cs
@@ -0,0 +1,54 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading;
+using System.Threading.Tasks;
+using System.Reflection;
+using System.Diagnostics;
+
+/*
+ * % make
+ * wait 1033msec
+ * await 103msec
+ * 
+ * % mono --version
+ * Mono JIT compiler version 4.0.3 (Stable 4.0.3.20/d6946b4 Tue Aug  4 09:43:57 UTC 2015)
+ * Copyright (C) 2002-2014 Novell, Inc, Xamarin Inc and Contributors. www.mono-project.com
+ *     TLS:           __thread
+ *     SIGSEGV:       altstack
+ *     Notifications: epoll
+ *     Architecture:  amd64
+ *     Disabled:      none
+ *     Misc:          softdebug 
+ *     LLVM:          supported, not enabled.
+ *     GC:            sgen
+*/
+
+public class WaitBehabiour
+{
+    static readonly List<int> loop = Enumerable.Range(1, 10).ToList();
+
+    public static void Main()
+    {
+        time("wait", waitCase);
+        time("await", awaitCase);
+    }
+
+    static void time(string name, Action f)
+    {
+        var t = Stopwatch.StartNew();
+        f();
+        t.Stop();
+        Console.WriteLine("{0} {1}msec", name, t.ElapsedMilliseconds);
+    }
+
+    static void waitCase()
+    {
+        loop.ForEach(_ => Task.Delay(100).Wait());
+    }
+
+    static void awaitCase()
+    {
+        Task.WaitAll(loop.Select(_ => Task.Delay(100)).ToArray());
+    }
+}
